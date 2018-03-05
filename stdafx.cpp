c70da0203f764d055d5c49983824ae0b8361ffe7
pvmoore <1926576+pvmoore@users.noreply.github.com>
2018-03-05 18:15:26 +0000

    remove statics

--- stdafx.cpp
+++ stdafx.cpp
@@ -7,3 +7,19 @@
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
 
+namespace core {
+
+// threads.h
+DWORD thread_wrapper_func(void* argsPtr) {
+	auto args = *(Thread::FuncWrapperArgs*)argsPtr;
+
+	// call the user function
+	DWORD exitCode = args.func(args.args);
+
+	// this thread is now exiting
+	args.t->setStatus(Thread::FINISHED);
+
+	return exitCode;
+}
+
+} /// core
